c591a6eb2fb612a2d3bba496b2c81e7984432fc2
UOghenejakpor@polarisbanklimited.com <UOghenejakpor@polarisbanklimited.com>
2021-02-22 18:46:00 +0100

    updates

--- PaymentEngine.Infrastructure/Utilities/RetryPolicy.cs
+++ PaymentEngine.Infrastructure/Utilities/RetryPolicy.cs
@@ -14,14 +14,12 @@ namespace PaymentEngine.Infrastructure.Utilities
                      .WaitAndRetryAsync(retryCount, retryAttempt =>
                      {
                          var timeToWait = TimeSpan.FromSeconds(retryAttempt * incrementalCount);
-                         Console.WriteLine($"Waiting {timeToWait.TotalSeconds} seconds..");
                          return timeToWait;
                      },
                        onRetry: (exception, pollyRetryCount, context) =>
                        {
-                           Console.WriteLine($"An exception occured at {exception.Source} with message {exception.Message}");
-                       })
-                     ;
+                       });
+                     
         }
     }
 }
